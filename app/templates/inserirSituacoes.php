04aa29fa412d979c886c8d4aef7aba5486748c4c
“dayvison99” <dayvison.marley@gmail.com>
2019-12-22 20:06:44 -0200

    alteraçoes tela inserção

--- app/templates/inserirSituacoes.php
+++ app/templates/inserirSituacoes.php
@@ -13,43 +13,61 @@
  <table cellspacing="10">
             </tr>
               <tr>
-              <td><h4>
-                    <label >Disciplinas :   </label>
-                  <h4>
+              <td><h3>
+                    <label for="status">Disciplina :   </label>
+                  <h3>
                 </td>
                     <td align="left" ><h4>
-
+                      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                      <select name="disciplina"class="select" input type="submit"  id ="disciplina">
                           {%for disciplina in disciplina   %}
-                          <tr class="table table-dark">
-                              <td><h4>  <a href='/analise/{{ disciplina.id }}'/>{{ disciplina.nome }}</a></h4></td>
-                          </tr>
+                            <option value={{ disciplina.nome }} >{{ disciplina.nome }}</option>
                           {% endfor %}
                    </select>
                     <h4></td>
            <tr>
-                <td><h4>
+                <td><h3>
                     <label for="status">Status :   </label>
                 </td>
                 <td align="left"><h4>
                     <select name="status"  onchange="chamar()" id="status">
                               <option value="nenhum" disabled selected>-----</option>
                               <option value="APROVADO">Aprovado</option>
-                              <option value="REPROVADO">Repovado</option>
+                              <option value="REPROVADO">Reprovado</option>
                               <option value="MATRICULADO">Matriculado</option>
                               <option value="CANCELADO">Desistente</option>
                     </select>
                </td>
+    <script type="text/javascript">
+    $('.select').on({change: listChildren}).trigger('change');
+function listChildren(){
+        if ( $(this).val() != '' ) {
+              children = $('option').val();
+              $(".input").hide();
+              $("#" + $(this).val() ).show();
+            }
+}
+    </script>
+    {%for periodo in periodo  %}
+  <script languagem="javascript" >
+    function chamar(){
+     var disci = document.getElementById("disciplina").value;
+     var status = document.getElementById("status").value;
+          alert([disci,status]);
+      }
+    </script>
+      {% endfor %}
    </td>
   </tr>
  </table>
 </fieldset>
 <br />
-<a href='/analise/'><button  title="Inserir! " class="btn btn-info btn-lg" type="button">ddFinalizar</button></a>
+<!--a href='/analise/'><button  title="Inserir! " class="btn btn-info btn-lg" type="button">ddFinalizar</button></a>
+-->
 
-{%for disciplina in disciplina   %}
-<a href='/analise/{{ disciplina.nome }}/'><button  title="Inserir! " class="btn btn-info btn-lg" type="button">Finalizar</button></a>
-{% endfor %}
-  <a href='/listagem/{{ disciplina.nome }}'><button  title="Inserir! " class="btn btn-info btn-lg" type="submit">INSERIR</button></a>
+<a href='/analise/{{ disciplina.nome}}/'><button  title="Inserir! " class="btn btn-info btn-lg" type="button">Finalizar</button></a>
+
+<a href='/listagem/{{ disciplina.nome }}'><button  title="Inserir! " class="btn btn-info btn-lg" type="submit">INSERIR</button></a>
 
 </form>
 </center></div>
